1af840eb7b531b7ea802919f353f7444f0f21372
edwin-hidalgo <59812889+edwin-hidalgo@users.noreply.github.com>
2020-01-12 22:17:46 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	$(".link_edwin").on("mouseover",function(){
+		$(".img_edwin").show()
+	}).on("mouseout",function(){
+		$(".img_edwin").hide()
+	}),
+
+	$(".link_blueno").on("mouseover",function(){
+		$(".img_blueno").show()
+	}).on("mouseout",function(){
+		$(".img_blueno").hide()
+	}),
+
+	$(".link_winter").on("mouseover",function(){
+		$(".img_winter").show()
+	}).on("mouseout",function(){
+		$(".img_winter").hide()
+	})
+
+});
